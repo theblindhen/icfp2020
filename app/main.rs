f784aedcfbd7250f7fcbdc6dd205c44ead335942
Johan Rosenkilde <jsrn@jsrn.dk>
2020-07-14 14:41:28 +0200

    Deserialize any deep JSON structure

--- app/main.rs
+++ app/main.rs
@@ -1,4 +1,6 @@
 use serde::Deserialize;
+use serde_json::Value;
+
 // This `derive` requires the `serde` dependency.
 #[derive(Deserialize, Debug)]
 struct Repo {
@@ -9,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Sending request...");
 
     // Demonstrates sending an HTTP request and decoding the response as JSON.
-    let repos : Vec<Repo> =
+    let repos : Value =
         ureq::get("http://jsrn.dk/advanced.json")
             .timeout(std::time::Duration::from_secs(30))
             .call()
