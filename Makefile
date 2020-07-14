eceffa10691b07cd4bbebaddbf53c3df1e845a0d
Johan Rosenkilde <jsrn@jsrn.dk>
2020-07-14 14:32:11 +0200

    Remove tls support to shrink vendor

--- Makefile
+++ Makefile
@@ -2,6 +2,18 @@ all:
 	@echo Use \`cargo\` to build.
 	@echo Use \`make submission\` to submit.
 
+.PHONY: docker-build
+docker-build: 
+    docker build -t icfp20 .
+
+.PHONY: docker-run
+docker-run: docker-build
+    docker run --rm icfp20
+
+.PHONY: docker-shell
+docker-shell: docker-build
+    docker run --rm -it --entrypoint /bin/bash icfp20
+
 submission:
 	git checkout submission
 	git pull
--- app/main.rs
+++ app/main.rs
@@ -1,5 +1,4 @@
 use serde::Deserialize;
-
 // This `derive` requires the `serde` dependency.
 #[derive(Deserialize, Debug)]
 struct Repo {
@@ -9,12 +8,9 @@ struct Repo {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Sending request...");
 
-    // Demonstrates sending an HTTP request and decoding the response as JSON. The GitHub API
-    // requires setting a User-Agent header, which means we can't use the `reqwest::blocking::get`
-    // convenience function.
+    // Demonstrates sending an HTTP request and decoding the response as JSON.
     let repos : Vec<Repo> =
-        ureq::get("https://api.github.com/orgs/theblindhen/repos")
-            .set("User-Agent", "Reqwest")
+        ureq::get("http://jsrn.dk/advanced.json")
             .timeout(std::time::Duration::from_secs(30))
             .call()
             .into_json_deserialize().unwrap();
