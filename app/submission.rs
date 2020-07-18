33f3146c5716b9e9f01ad35894205b9f5bffa0ad
Kasper Svendsen <kaspersv@gmail.com>
2020-07-18 18:21:53 +0200

    used modulated encoding when sending join msg

--- app/submission.rs
+++ app/submission.rs
@@ -1,30 +1,42 @@
 #![allow(unused)]
 
+mod aplang;
+mod bits2d;
+mod draw;
+mod encodings;
+mod interpreter;
+mod lexer;
+
 use log::*;
 use std::env;
 
 fn post(url: &str, body: &str) -> Result<String, Box<dyn std::error::Error>> {
-    println!("Sending POST request with body {} to {}...", body, url);
+    println!("Sending POST request with body {} to {}", body, url);
 
-    let reply =
-        ureq::post(url)
-            .timeout(std::time::Duration::from_secs(30))
-            .send_string(body)
-            .into_string()?;
+    let reply = ureq::post(url)
+        .timeout(std::time::Duration::from_secs(30))
+        .send_string(body)
+        .into_string()?;
     Ok(reply)
+}
+
+fn join_msg(player_key: i64) -> String {
+    use encodings::{vcons, vint, vnil};
+
+    let join = vcons(vint(2), vcons(vint(player_key), vcons(vnil(), vnil())));
+    encodings::modulate(&join)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     let server_url = &args[1];
-    let player_key = &args[2];
+    let player_key: i64 = args[2].parse().unwrap();
 
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
 
     let url = format!("{}/aliens/send", server_url);
-    let body = format!("(2, {}, nil)", player_key);
-    let reply = post(&url, &body)?;
+    let reply = post(&url, &join_msg(player_key))?;
 
     println!("Reply: {}", reply);
     Ok(())
